67bbf0d85716f690e2ce3b33503bd8e0b3718c2d
Taras <47023611+tkvitko@users.noreply.github.com>
2023-04-30 07:42:57 +0300

    Revert "homework 5 done"

--- 4_django_project/shop/goods/forms.py
+++ 4_django_project/shop/goods/forms.py
@@ -6,10 +6,4 @@ from .models import Good
 class GoodForm(ModelForm):
     class Meta:
         model = Good
-        fields = '__all__'
-
-    def __init__(self, *args, **kwargs):
-        super(GoodForm, self).__init__(*args, **kwargs)
-        for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'form-control'
-            field.help_text = ''
+        fields = ['name', 'price', 'unit', 'vendor']
--- 4_django_project/shop/goods/views.py
+++ 4_django_project/shop/goods/views.py
@@ -1,7 +1,5 @@
 from django.contrib import messages
-from django.http import JsonResponse
 from django.shortcuts import render, redirect
-from django.template.loader import render_to_string
 
 from .models import Good
 from .forms import GoodForm
@@ -13,38 +11,15 @@ def list_view(request):
 
 
 def create_good(request):
-    data = dict()
     if request.method == "POST":
         good_form = GoodForm(request.POST)
         if good_form.is_valid():
             good_form.save()
-            data['form_is_valid'] = True
-            goods = Good.objects.all()
-            data['products_html'] = render_to_string('goods_list.html', {'goods': goods})
-            # messages.success(request, 'Your good was successfully added!')
+            messages.success(request, 'Your good was successfully added!')
         else:
-            # messages.error(request, 'Error saving form')
-            data['form_html'] = render_to_string('form.html', {'form': good_form}, request=request)
-        # return redirect("goods")
+            messages.error(request, 'Error saving form')
+        return redirect("goods")
     else:
-        data['form_is_valid'] = False
-        data['form_html'] = render_to_string('form.html', {'form': GoodForm()}, request=request)
-
-    return JsonResponse(data)
-
-
-# def save_good_form(request, form, template_name):
-#     data = dict()
-#     if request.method == 'POST':
-#         if form.is_valid():
-#             form.save()
-#             data['form_is_valid'] = True
-#             goods = Good.objects.all()
-#             data['html_good_list'] = render_to_string('good_list.html', {
-#                 'goods': goods
-#             })
-#         else:
-#             data['form_is_valid'] = False
-#     context = {'form': form}
-#     data['html_form'] = render_to_string(template_name, context, request=request)
-#     return JsonResponse(data)
+        good_form = GoodForm()
+        # context = {"goods": Good.objects.all()}
+        return render(request, "good_create.html", {"form": good_form})
